719221a008340ad00d1593c48e071ebb7c6c9c8e
dhiya-adhas90 <100505746+DhiyaAdha@users.noreply.github.com>
2024-06-26 23:07:35 +0700

    up schema category&img

---
+++ models/Image.js
@@ -0,0 +1,10 @@
+const { name } = require("ejs");
+const mongoose = require("mongoose");
+
+const imageSchema = new mongoose.Schema({
+  imageUrl: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Image", imageSchema);
