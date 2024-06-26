9cf1a1802690f2c6f7ff8120049a7827b0dd58ba
dhiya-adhas90 <100505746+DhiyaAdha@users.noreply.github.com>
2024-06-26 23:42:25 +0700

    up schema all model

---
+++ models/Booking.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const bookingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    default: "Indonesia",
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  isPopular: {
+    type: Boolean,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageId: [
+    {
+      type: ObjectId,
+      ref: "Image",
+    },
+  ],
+  featured: [
+    {
+      type: ObjectId,
+      ref: "Feature",
+    },
+  ],
+  activityId: [
+    {
+      type: ObjectId,
+      ref: "Activity",
+    },
+  ],
+});
+
+module.exports = mongoose.model("con", bookingSchema);
